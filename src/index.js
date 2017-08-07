355dff3008fb8b359f7ec521b4eccd16aae8f96c
Kenton Glass <hellokenton@gmail.com>
2017-08-07 14:40:54 -0400

    Added Scss support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import './css/style.css';
+
 import App from './components/App';
 
 render(<App />, document.getElementById('root'));

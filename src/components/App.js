ae0ba31a655e2307d2cb2aaf2969dfb5480e0e21
Kenton Glass <hellokenton@gmail.com>
2017-08-07 15:02:52 -0400

    Added react-router-dom and set up basic components and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ReactGA from 'react-ga';
+
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
+
+ReactGA.initialize('UA-20333926-13');
 
 class App extends Component {
   render() {
     return (
-      <div>
-        RageKage
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ReactGA from 'react-ga';
 
 import './css/style.css';
 
 import App from './components/App';
-
-ReactGA.initialize('UA-20333926-13');
 
 render(<App />, document.getElementById('root'));

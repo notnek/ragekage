4fed6b063a077a50d393724a53d72aa4c3d9e779
Kenton Glass <hellokenton@gmail.com>
2017-08-07 14:45:59 -0400

    Add react-ga for analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import ReactGA from 'react-ga';
 
 import './css/style.css';
 
 import App from './components/App';
+
+ReactGA.initialize('UA-20333926-13');
 
 render(<App />, document.getElementById('root'));

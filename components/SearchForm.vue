8bcda3aab77d2803fb10ac7391e13eff67f3ebf5
Kenton Glass <notnek@users.noreply.github.com>
2020-07-20 09:09:29 -0400

    Update light mode form styles

--- components/SearchForm.vue
+++ components/SearchForm.vue
@@ -2,7 +2,7 @@
   <form class="mb-6" @submit.prevent>
     <input
       v-model="query"
-      class="focus:outline-none px-4 py-3 placeholder-gray-700 text-gray-900 w-full dark-mode:bg-gray-800 dark-mode:placeholder-gray-500 dark-mode:text-gray-300 border border-solid border-transparent focus:border-white dark-mode:focus:border-gray-500"
+      class="focus:outline-none px-4 py-3 placeholder-gray-700 text-gray-900 w-full dark-mode:bg-gray-800 dark-mode:placeholder-gray-500 dark-mode:text-gray-300 border border-solid border-gray-400 dark-mode:border-transparent focus:border-gray-800 dark-mode:focus:border-gray-500"
       placeholder="Type to find a gif..."
       type="search"
     />
